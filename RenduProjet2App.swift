2916b829143926dadb2ab471c8517309c4502d13
RENAUD Brévin <renaudbrevin@gmail.com>
2024-10-17 14:28:56 +0200

    Add files via upload

---
+++ RenduProjet2App.swift
@@ -0,0 +1,17 @@
+//
+//  RenduProjet2App.swift
+//  RenduProjet2
+//
+//  Created by RENAUD Brévin on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RenduProjet2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
